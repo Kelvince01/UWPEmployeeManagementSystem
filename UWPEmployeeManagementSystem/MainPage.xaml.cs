107ba3d6b805fb41494bad8136518edb6b2913d6
Kelvince1 <themirkochip1@gmail.com>
2021-05-24 18:07:23 +0300

    Add project files.

---
+++ UWPEmployeeManagementSystem/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MySql.Data.MySqlClient;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPEmployeeManagementSystem
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            string M_str_sqlcon = "server=localhost;user id=root;password=;database=retailmanagementsystem";
+            MySqlConnection mysqlcon = new MySqlConnection(M_str_sqlcon);
+            MySqlCommand mysqlcom = new MySqlCommand("select * from users", mysqlcon);
+            mysqlcon.Open();
+            MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+            while (mysqlread.Read())
+            {
+                Debug.WriteLine(mysqlread.GetString(0) + ":" + mysqlread.GetString(1));
+            }
+            mysqlcon.Close();
+        }
+    }
+}
